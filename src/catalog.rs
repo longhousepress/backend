9f7865de50373de46e460d366a1a09dcd037492f
publius <info@longhousepress.org>
2026-03-07 22:52:19 +0000

    Fixed catalog path bug

--- src/catalog.rs
+++ src/catalog.rs
@@ -1,12 +1,13 @@
 use rocket::{State, http::Status, serde::json::Json};
 use sqlx::SqlitePool;
 
+use crate::config::Config;
 use crate::db::load_books;
 use crate::models::Book;
 
 #[get("/books", rank = 1)]
-pub async fn books(db: &State<SqlitePool>) -> Result<Json<Vec<Book>>, Status> {
-    match load_books(db).await {
+pub async fn books(db: &State<SqlitePool>, config: &State<Config>) -> Result<Json<Vec<Book>>, Status> {
+    match load_books(db, &config.static_dir).await {
         Ok(books) => Ok(Json(books)),
         Err(e) => {
             rocket::error!("Failed to load books catalog: {}", e);
